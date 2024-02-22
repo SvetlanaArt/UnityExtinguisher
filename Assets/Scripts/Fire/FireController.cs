097e50a4dc91974ffc56d40e60d1fdbc871b9f7a
Svetlana Artsiomenka <148551551+SvetlanaArt@users.noreply.github.com>
2024-02-22 15:41:59 +0300

    Optimize model data
    Optimize model data.
    Fixed bug with Fire data

--- Assets/Scripts/Fire/FireController.cs
+++ Assets/Scripts/Fire/FireController.cs
@@ -22,7 +22,7 @@ public class FireController : MonoBehaviour
     // maximum level from start
     float maxFireLevel;
 
-    void Start()
+    void Awake()
     {
         InitComponents();
         InitFireLevel();
